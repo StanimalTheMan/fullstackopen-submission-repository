400ecb2ee3859b498140540b6ceec6459a9ab48c
StanimalTheMan <sjc591@nyu.edu>
2021-04-15 01:02:14 -0400

    2.14 failed attempt

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -5,14 +5,25 @@ const App = () => {
   const [countries, setCountries] = useState([]);
   const [countriesFilter, setCountriesFilter] = useState("");
   const [countrySelected, setCountrySelected] = useState("");
+  const [countrySelectedWeather, setCountrySelectedWeather] = useState({});
 
   useEffect(() => {
     axios.get("https://restcountries.eu/rest/v2/all").then((response) => {
-      console.log("promise fulfilled");
-      console.log(response.data);
+      // console.log("promise fulfilled");
+      // console.log(response.data);
       setCountries(response.data);
     });
   }, []);
+
+  useEffect(() => {
+    axios
+      .get(
+        `http://api.weatherstack.com/current?access_key=${process.env.REACT_APP_API_KEY}&query=${countrySelected.capital}`
+      )
+      .then((response) => {
+        setCountrySelectedWeather(response.data);
+      });
+  }, [countrySelected]);
 
   const handleChange = (e) => {
     setCountriesFilter(e.target.value);
@@ -38,27 +49,6 @@ const App = () => {
         <button onClick={() => showCountryDetails(country)}>show</button>
       </li>
     ));
-    if (countrySelected) {
-      userFeedback = (
-        <div>
-          <h1>{countrySelected.name}</h1>
-          <p>capital {countrySelected.capital}</p>
-          <p>population {countrySelected.population}</p>
-          <h2>languages</h2>
-          <ul>
-            {countrySelected.languages.map((language) => (
-              <li key={language.name}>{language.name}</li>
-            ))}
-          </ul>
-          <img
-            src={countrySelected.flag}
-            alt={countrySelected.name}
-            width={50}
-            height={50}
-          />
-        </div>
-      );
-    }
   } else if (countriesToShowLength === 1) {
     let country = countriesToShow[0];
     userFeedback = (
@@ -73,6 +63,36 @@ const App = () => {
           ))}
         </ul>
         <img src={country.flag} alt={country.name} width={50} height={50} />
+      </div>
+    );
+  }
+  if (countrySelected && countrySelectedWeather) {
+    console.log(countrySelectedWeather);
+    userFeedback = (
+      <div>
+        <h1>{countrySelected.name}</h1>
+        <p>capital {countrySelected.capital}</p>
+        <p>population {countrySelected.population}</p>
+        <h2>languages</h2>
+        <ul>
+          {countrySelected.languages.map((language) => (
+            <li key={language.name}>{language.name}</li>
+          ))}
+        </ul>
+        <img
+          src={countrySelected.flag}
+          alt={countrySelected.name}
+          width={50}
+          height={50}
+        />
+        <h2>Weather in {countrySelected.capital}</h2>
+        <p>
+          <strong>temperature:</strong>{" "}
+          {countrySelectedWeather.current.temperature} Celsius
+        </p>
+        <p>
+          <strong>wind: </strong>
+        </p>
       </div>
     );
   }
